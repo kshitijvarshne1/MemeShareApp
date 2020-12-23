0432b1c78f307ba0eb054022a71094831fb2b2cc
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-24 00:35:23 +0530

    add volley method and extension to MainActivity.kt

--- app/src/main/java/varshney/kshitij/memeshareapp/MainActivity.kt
+++ app/src/main/java/varshney/kshitij/memeshareapp/MainActivity.kt
@@ -3,13 +3,31 @@ package varshney.kshitij.memeshareapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    private fun loadMeme(){
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
 
+.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+
+            },
+            Response.ErrorListener {  })
+
+
+        queue.add(stringRequest)
+    }
     fun shareMeme(view: View) {}
     fun nextMeme(view: View) {}
 }

a79025e6d64bb01058841d456fd87fce544f5de6
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-24 00:46:45 +0530

    add jsonObjectRequest to MainActivity.kt

--- app/src/main/java/varshney/kshitij/memeshareapp/MainActivity.kt
+++ app/src/main/java/varshney/kshitij/memeshareapp/MainActivity.kt
@@ -2,9 +2,11 @@ package varshney.kshitij.memeshareapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 
@@ -12,21 +14,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadMeme()
     }
     private fun loadMeme(){
         val queue = Volley.newRequestQueue(this)
         val url = "https://meme-api.herokuapp.com/gimme"
 
 
-        val stringRequest = StringRequest(
-            Request.Method.GET, url,
-            Response.Listener<String> { response ->
-
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url,null,
+            Response.Listener { response ->
+                val url=response.getString("url")
             },
-            Response.ErrorListener {  })
+            Response.ErrorListener {
+
+            })
 
 
-        queue.add(stringRequest)
+        queue.add(jsonObjectRequest)
     }
     fun shareMeme(view: View) {}
     fun nextMeme(view: View) {}

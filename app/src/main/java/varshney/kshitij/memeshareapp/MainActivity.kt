7126a6b833ac2a0ef50b107e6809b186d7de949a
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-23 18:42:11 +0530

    Initial commit

---
+++ app/src/main/java/varshney/kshitij/memeshareapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package varshney.kshitij.memeshareapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun shareMeme(view: View) {}
+    fun nextMeme(view: View) {}
+}
